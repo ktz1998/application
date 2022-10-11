bfe68a43c14535a7c7246258a6bca0c06ce6a601
ktz1998 <doraemonfan.kosuke@gmail.com>
2022-10-11 08:15:13 +0000

    End 2章

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "4d1408d649fc4736a7d6977ef8a3f057.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
+
 end
