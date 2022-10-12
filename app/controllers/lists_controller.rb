5b9d442adfaf8578224efbc972d5adadf9dfd85e
ktz1998 <doraemonfan.kosuke@gmail.com>
2022-10-12 02:28:57 +0000

    End 9章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,14 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  # 以下を追加
-  def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
-  end
+ def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+ end
 
   def index
     @lists = List.all
@@ -22,26 +24,17 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
-  def create
-    @list = List.new(list_params)
-    if @list.save
-      redirect_to list_path(@list.id)
-    else
-      render :new
-    end 
-  end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
-    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   private
