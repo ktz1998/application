27507888cf5c6c7f085fae9331cbb363fe49b273
ktz1998 <doraemonfan.kosuke@gmail.com>
2022-10-12 01:54:01 +0000

    End 8章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,10 +22,31 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+  
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end 
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :bod, :image)
   end
 end
